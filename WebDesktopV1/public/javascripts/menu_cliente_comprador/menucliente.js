48fda9237c046b89a7114cbd029b1b95be4ae5fb
MariaAPaezT@gmail.com <MariaAPaezT@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-02-25 05:24:15 +0000

    modificada el ciente comprador, agregada el detalle de indicadores, indicadores. Cabe destacar que las graficas aun no abren y falta instancias todo que corresponde al portal ejecutivo del cliente

--- WebDesktopV1/public/javascripts/menu_cliente_comprador/menucliente.js
+++ WebDesktopV1/public/javascripts/menu_cliente_comprador/menucliente.js
@@ -1,26 +1,23 @@
-  //Calcular las posiciones x,y segun la resolucion del monitor del cliente para el tabpanel por ser un layout absolute
-   var posx = 0;
-   var posy = 0;
-   var factorw = 0.001;
-   var factorh = 0.10;
-   //1024x768
-   if (screen.width==1024 && screen.height==768)
-   {
-    factorw = 0.025;
-    factorh = 0.10;
-   }
-   //1280x800
-   if (screen.width==1280 && screen.height==800)
-   {
-    factorw = 0.080;
-    factorh = 0.10;
-   }
-   //1280x1024
-   if (screen.width==1280 && screen.height==1024)
-   {
-    factorw = 0.020;
-    factorh = 0.005;
-   }
+//Calcular las posiciones x,y segun la resolucion del monitor del cliente para el tabpanel por ser un layout absolute
+var posx = 0;
+var posy = 0;
+var factorw = 0.001;
+var factorh = 0.10;
+//1024x768
+if (screen.width == 1024 && screen.height == 768) {
+	factorw = 0.025;
+	factorh = 0.10;
+}
+//1280x800
+if (screen.width == 1280 && screen.height == 800) {
+	factorw = 0.080;
+	factorh = 0.10;
+}
+//1280x1024
+if (screen.width == 1280 && screen.height == 1024) {
+	factorw = 0.020;
+	factorh = 0.005;
+}
 posx = parseInt(screen.width * factorw);
 posy = parseInt(screen.height * factorh);
 
@@ -29,32 +26,35 @@ Ext.require(['Ext.tree.*', 'Ext.data.*', 'Ext.tip.*', 'Ext.container.Viewport', 
 Ext.onReady(function() {
 	Ext.QuickTips.init();
 	var store = Ext.create('Ext.data.TreeStore', {
-	proxy: {
-	type: 'ajax',
-	url: 'cli_comprador/generarmenu?tipo=3'
-	},
-	root: {
-	text: 'Menu Principal',
-	id: 'root_node',
-	expanded: true
-	},
-	folderSort: false,
-	sorters: [{
-	property: 'id',
-	direction: 'ASC'
-	}],
-});
-ventana = Ext.create('miVentanalista');
-ventana.show();
+		proxy : {
+			type : 'ajax',
+			url : 'cli_comprador/generarmenu?tipo=3'
+		},
+		root : {
+			text : 'Menu Principal',
+			id : 'root_node',
+			expanded : true
+		},
+		folderSort : false,
+		sorters : [{
+			property : 'id',
+			direction : 'ASC'
+		}],
+	});
+	ventana = Ext.create('miVentanalista');
+	ventana.show();
 
-var tree = Ext.create('Ext.tree.Panel', {
-	store : store,
-	renderTo : 'tree_el',
-	height : 500,
-	width : 330,
-	collapsible : true,
-	title : 'Menu AEVEV ',
-	dockedItems : [{
+	ventanaindicadores = Ext.create('indicadoreseje');
+	ventanaindicadores.show();
+
+	var tree = Ext.create('Ext.tree.Panel', {
+		store : store,
+		renderTo : 'tree_el',
+		height : 500,
+		width : 330,
+		collapsible : true,
+		title : 'Menu AEVEV ',
+		dockedItems : [{
 			xtype : 'toolbar',
 			items : [{
 				text : 'Opciones',
@@ -63,73 +63,65 @@ var tree = Ext.create('Ext.tree.Panel', {
 				}
 			}]
 		}],
-	useArrows : true,
-	listeners : {
-		itemclick : function(view, node) {
-			if (node.get('text') == "Actualizar Datos Personales") {
-				ventana.close();
-				ventana = Ext.create('actualizar');
-				ventana.show();
-			}
-			if (node.get('text') == "Lista de Espera")
-			 {
-				ventana.close()
-				ventana = Ext.create('miVentanalista');
-				ventana.show();
-			}
-			if (node.get('text') == "Ford") // (node.get('Text') == "Fiat") )//|| node.get('Text') == "Daewood" || node.get('Text') == "Toyota" || node.get('Text') == "Huyndai")
-			 {
-				ventana.close();
-				ventana =Ext.create('catalogo');
-				ventana.show();
-			}
-			if (node.get('text') == "Fiat")
-			{
-				ventana.close();
-				ventana =Ext.create('catalogo');
-				ventana.show();
-			}
-			if (node.get('text') == "Daewood")
-			{
-				ventana.close();
-				ventana =Ext.create('catalogo');
-				ventana.show();
-			}
-			if (node.get('text') == "Toyota")
-			{
-				ventana.close();
-				ventana =Ext.create('catalogo');
-				ventana.show();
-			}
-			if (node.get('text') == "Huyndai")
-			{
-				ventana.close();
-				ventana =Ext.create('catalogo');
-				ventana.show();
-			}
-			if (node.get('text') == "Anular Pedido")
-			 {
-				ventana.close();
-				ventana =Ext.create('anularpedido');
-				ventana.show();
-			}
-			if (node.get('text') == "Proforma")
-			 {
-				ventana.close();
-				ventana =Ext.create('proformas');
-				ventana.show();
-			 }
-			if (node.get('text') == "Comprar Vehiculo") 
-			{
-				ventana.close();
-				ventana = Ext.create('comprarvehiculo');
-				ventana.show();
+		useArrows : true,
+		listeners : {
+			itemclick : function(view, node) {
+				if (node.get('text') == "Actualizar Datos Personales") {
+					ventana.close();
+					ventana = Ext.create('actualizar');
+					ventana.show();
+				}
+				if (node.get('text') == "Lista de Espera") {
+					ventana.close()
+					ventana = Ext.create('miVentanalista');
+					ventana.show();
+				}
+				if (node.get('text') == "Ford")// (node.get('Text') == "Fiat") )//|| node.get('Text') == "Daewood" || node.get('Text') == "Toyota" || node.get('Text') == "Huyndai")
+				{
+					ventana.close();
+					ventana = Ext.create('catalogo');
+					ventana.show();
+				}
+				if (node.get('text') == "Fiat") {
+					ventana.close();
+					ventana = Ext.create('catalogo');
+					ventana.show();
+				}
+				if (node.get('text') == "Daewood") {
+					ventana.close();
+					ventana = Ext.create('catalogo');
+					ventana.show();
+				}
+				if (node.get('text') == "Toyota") {
+					ventana.close();
+					ventana = Ext.create('catalogo');
+					ventana.show();
+				}
+				if (node.get('text') == "Huyndai") {
+					ventana.close();
+					ventana = Ext.create('catalogo');
+					ventana.show();
+				}
+				if (node.get('text') == "Anular Pedido") {
+					ventana.close();
+					ventana = Ext.create('anularpedido');
+					ventana.show();
+				}
+				if (node.get('text') == "Proforma") {
+					ventana.close();
+					ventana = Ext.create('proformas');
+					ventana.show();
+				}
+				if (node.get('text') == "Comprar Vehiculo") {
+					ventana.close();
+					ventana = Ext.create('comprarvehiculo');
+					ventana.show();
+				}
 			}
 		}
-	}
-});
+	});
 
-   // ventana = Ext.create('listaespera');
+	// ventana = Ext.create('listaespera');
 	//ventana.show();
 
-});
+}); 
