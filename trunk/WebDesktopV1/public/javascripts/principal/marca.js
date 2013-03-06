a38c12d945cb04583a8d5f7a68e8297e5433bf37
adrianadelvalle90 <adrianadelvalle90@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-06 23:01:17 +0000

    Se modifico el js de marca y el js de inicio funciona a medias

--- trunk/WebDesktopV1/public/javascripts/principal/marca.js
+++ trunk/WebDesktopV1/public/javascripts/principal/marca.js
@@ -1,107 +1,48 @@
-     var tabs = null;
+function buscar(id_marca) {
+	var marca123 = "hola";
+	var ajax = Ext.Ajax.request({
+		url : '/inicio/buscar',
+		//Enviando los parametros a la pagina servidora
+		params : {
+			ajax : 'true',
+			funcion : 'buscar',
+			id_marca : id_marca
+		},
+		//Retorno exitoso de la pagina servidora a traves del formato JSON
+		success : function(resultado, request) {
+			datos = Ext.JSON.decode(resultado.responseText);
+			if (datos.exito == 'true') {
+				marca123 = datos.nombre_marca;
+				Ext.create('MyApp.view.MyTabPanel', {
+					renderTo : 'tree_el',
+					title : datos.nombre_marca,
+					items : [{
+						xtype : 'panel',
+						title : 'Quienes Somos',
+						html : '<h1>' + datos.mision + '</h1><br><h1>' + datos.vision + '</h1><br><h1>' + datos.valores + '</h1><br>'
+					}, {
+						xtype : 'panel',
+						title : 'Concesionarios',
+						//html  : '<h1></h1><h1></h1><div align="center"><img src="images/mapa_mundi.png""></div>'
+						html : '<iframe width="700" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.es/maps?f=d&amp;source=s_d&amp;saddr=Venezuela&amp;daddr=&amp;hl=es&amp;geocode=&amp;aq=0&amp;oq=vene&amp;sll=40.396764,-3.713379&amp;sspn=8.766144,21.643066&amp;mra=ls&amp;num=10&amp;ie=UTF8&amp;ll=6.42375,-66.58973&amp;spn=17.780303,17.893559&amp;t=m&amp;output=embed"></iframe><br /><small><a href="https://maps.google.es/maps?f=d&amp;source=embed&amp;saddr=Venezuela&amp;daddr=&amp;hl=es&amp;geocode=&amp;aq=0&amp;oq=vene&amp;sll=40.396764,-3.713379&amp;sspn=8.766144,21.643066&amp;mra=ls&amp;num=10&amp;ie=UTF8&amp;ll=6.42375,-66.58973&amp;spn=17.780303,17.893559&amp;t=m" style="color:#0000FF;text-align:left">Ver mapa más grande</a></small>',
+					}, {
+						xtype : 'panel',
+						title : 'Contacto',
+						html : '<h1>Contáctanos</h1><p>' + datos.contacto + '</p>'
 
-   //Punto de entrada a la aplicacion
-   Ext.onReady(function() {
-
-   Ext.QuickTips.init();
-     
-   //Definicion de la clase MiPanel
-   Ext.define('App.MiPanel', {
-    extend: 'Ext.form.Panel',
-    
-    initComponent : function() {
-      this.dockedItems = [ {
-        dock: 'bottom',
-        buttons: [{
-         text: 'Comprar',
-         formBind: true, // Disponible hasta que los campos esten validados
-         disabled: true,
-         handler: function() {
-          enviar();  
-         }
-        }]
-        } ];
-     //Llamamos a la super clase a iniciacion del componente
-     App.MiPanel.superclass.initComponent.call(this);
-    }
-    
-   });
-
-   //Definicion de la clase MiPanelUsuario
-   Ext.define('App.MiPanelMarca', {
-    extend: 'App.MiPanel',
-
-    //Definicion del alias que puede usado en un xtype
-    alias: 'widget.mipanelgaleria',
-    id: 'mipanelgaleria',
-    bodyPadding: 5,
-    width: 350,
-
-    //Sobre escribimos este metodo de Ext.grid.Panel
-    initComponent : function() {
-        //Definicion del panel
-        // this.items = [
-         // {
-          // fieldLabel: 'Usuario',
-          // xtype: 'textfield',
-          // name: 'usuario',
-          // id: 'usuario',
-          // allowBlank: false
-         // },{
-          // fieldLabel: 'Clave',
-          // xtype: 'textfield',
-          // name: 'clave',
-          // id: 'clave',
-          // inputType: 'password',
-          // allowBlank: false,
-    // listeners: {
-                      // specialkey: function(field, e){
-                       // if (e.getKey() == e.ENTER) {
-                         // enviar();
-                       // }
-                      // }
-                    // }
-         // }
-        // ]
-        ;
-
-        //Llamamos a la super clase a iniciacion del componente
-        App.MiPanelMarca.superclass.initComponent.call(this);
-    }
-   });
-   
-   tabs = Ext.createWidget('tabpanel', {
-        id: 'mitabpanel',
-        // renderTo: 'page',
-        layout: 'absolute',
-        title: 'Acceso',
-        x: 325,
-        y: -500,
-        width: 800,
-        height: 500,
-        activeTab: 0,
-        resizeTabs: true,
-        enableTabScroll: true,
-        
-        closeAction :'hide',
-        plain       : true,
-        closable    : true,
-        colapsable  : true,
-        resizable   : true,
-        modal       : false,
-        
-        defaults: {
-            autoScroll:true,
-            bodyPadding: 10
-        },
-        items: [{
-            title: 'Galeria',
-            xtype: 'mipanelgaleria',
-      itemId: 'mipanelu'
-        }],
-        renderTo: 'formularios'
-    });
-
-    Ext.getCmp('usuario').focus();
-
-   });
+					}, {
+						stype : 'panel',
+						title : 'Galeria de Vehiculos',
+						html : '<div align="center"><img src="' + datos.imagen + '"></div>'
+					}]
+				});
+			} else {
+				Ext.Msg.alert("Error", "paso");
+			}
+		},
+		//No hay retorno de la pagina servidora
+		failure : function() {
+			Ext.Msg.alert("Error", "Servidor no conectado");
+		}
+	});
+}
