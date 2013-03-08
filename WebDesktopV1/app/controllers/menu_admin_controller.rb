19e22f8a1cac8d605eea201317a0f1cb18b4d60a
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-08 00:14:02 +0000

    funcion grabar_marca  YA guarda xD

--- WebDesktopV1/app/controllers/menu_admin_controller.rb
+++ WebDesktopV1/app/controllers/menu_admin_controller.rb
@@ -1,11 +1,23 @@
 class MenuAdminController < ApplicationController
   def index
   end
-  
+
   def generar_menu
     @tipo=params[:tipo]
     @arbols = Portal_ejecutivo_desktops.new
     @tira = @arbols.BuscarTodosArbolJson(@tipo)
     render :text => @tira
+  end
+
+  def grabar_marca
+    @marca= Marca.new
+    @nombre=params[:nombre]
+    @imagen=params[:imagen]
+    @mision=params[:mision]
+    @vision=params[:vision]
+    @valores=params[:valores]
+    @contacto=params[:contacto]
+    @marca.grabar_marca(@nombre,@imagen,@mision,@vision,@valores,@contacto)
+    render :text => $tirajson
   end
 end
