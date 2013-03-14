cbcac7f34b85a5ddbcfe75f7e981608dad913cfa
adrianadelvalle90 <adrianadelvalle90@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-14 18:02:22 +0000

    Se arreglo el problema de la session ocacionada por el nuevo script de la base de datos que envio el dream team

--- trunk/WebDesktopV1/app/controllers/application_controller.rb
+++ trunk/WebDesktopV1/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def cerrarSession
-    session[:nombre_login]=nil
+    session[:nombre]=nil
     redirect_to '/inicio'
   end
 end
