4b8bd463dfe17ae569e5a5c9320b52a61d59f187
adrianadelvalle90 <adrianadelvalle90@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-15 21:50:56 +0000

    Se arreglo el rol_id de los controladores y se agregaron las funciones para que cargue la informacion del usuario

--- trunk/WebDesktopV1/app/controllers/menu_ensambladora_controller.rb
+++ trunk/WebDesktopV1/app/controllers/menu_ensambladora_controller.rb
@@ -2,11 +2,11 @@ class MenuEnsambladoraController < ApplicationController
   def index
     if(session[:nombre]==nil)
       redirect_to '/inicio'
-    elsif(session[:rol_id]=='1')
+    elsif(session[:rol_id]==1)
       redirect_to '/cli_comprador'
-    elsif(session[:rol_id]=='2')
+    elsif(session[:rol_id]==2)
       redirect_to '/concesionario'
-    elsif(session[:rol_id]=='4')
+    elsif(session[:rol_id]==4)
       redirect_to '/menu_admin'
     end
   end
