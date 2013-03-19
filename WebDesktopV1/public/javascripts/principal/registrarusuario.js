32931defce56943e358d5eb049d81b77dffd5623
jmgv1989@gmail.com <jmgv1989@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-19 04:38:35 +0000

    dimensiones del js ajustadas a un tamaño general, la distribucion de los campos aun se debe revisar , se agrego el campo de repetir contraseña para seguir el estandar del las paginas web

--- WebDesktopV1/public/javascripts/principal/registrarusuario.js
+++ WebDesktopV1/public/javascripts/principal/registrarusuario.js
@@ -26,152 +26,133 @@ posy = parseInt(screen.height * factorh);
 
 Ext.onReady(function(){
 		
-	Ext.define('registrarusuario', {
-    extend: 'Ext.window.Window',
-
-    height: 293,
-    width: 660,
-    title: 'Ventana de registro',
-
-    initComponent: function() {
-        var me = this;
-
-        Ext.applyIf(me, {
-            items: [
-                {
-                    xtype: 'form',
-                    height: 254,
-                    width: 637,
-                    layout: {
-                        type: 'absolute'
-                    },
-                    bodyPadding: 10,
-                    title: 'Crear cuanta de usuario',
-                    items: [
-                        {
+	Ext.define('VentaRegistrarUsuario', {
+		extend : 'Ext.window.Window',
+		
+		height : 439,
+		width : 635, 
+		y : 180,
+		layout : {
+			type : 'absolute'
+		},
+		title : 'Registrarse',
+		items : [
+						{
                             xtype: 'textfield',
-                            x: 20,
-                            y: 20,
+                            x: 0,
+                            y: 70,
                             width: 200,
                             fieldLabel: 'Cedula',
                             id : 'cedula'
-                        },
-                        {
+                        },  {
                             xtype: 'textfield',
-                            x: 20,
-                            y: 50,
+                            x: 0,
+                            y: 110,
                             width: 290,
                             fieldLabel: 'Nombres',
                             id : 'nombres'
-                        },
-                        {
+                        }, {
                             xtype: 'textfield',
-                            x: 20,
-                            y: 80,
+                            x: 0,
+                            y: 150,
                             width: 290,
                             fieldLabel: 'Apellidos',
                             id : 'apellidos'
-                        },
-                        {
+                        }, {
                             xtype: 'textfield',
-                            x: 20,
-                            y: 110,
+                            x: 0,
+                            y: 190,
                             width: 250,
                             fieldLabel: 'Telefono',
                             id : 'telefono'
-                        },
-                        {
+                        }, {
                             xtype: 'textfield',
-                            x: 340,
-                            y: 100,
+                            x: 260,
+                            y: 150,
                             fieldLabel: 'Correo',
                             id : 'correo'
-                        },
-                        {
+                        },{
                             xtype: 'textfield',
-                            x: 20,
-                            y: 140,
+                            x: 260,
+                            y: 190,
+                            fieldLabel: 'Repetir Correo',
+                            id : 'correo2'
+                        },{
+                            xtype: 'textfield',
+                            x: 0,
+                            y: 230,
                             width: 430,
                             fieldLabel: 'Direccion',
                             id : 'direccion'
-                        },
-                        {
+                        },{
                             xtype: 'datefield',
-                            x: 340,
-                            y: 20,
+                            x: 260,
+                            y: 70,
                             fieldLabel: 'Fecha de nacimiento',
                             id : 'fecha_nacimiento'
-                        },
-                        {
+                        }, {
                             xtype: 'combobox',
-                            x: 340,
-                            y: 60,
+                            x: 260,
+                            y: 110,
                             width: 200,
                             fieldLabel: 'Sexo',
                             id : 'sexo'
-                        },
-                        {
+                        }, {
                             xtype: 'textfield',
-                            x: 20,
-                            y: 170,
+                            x: 0,
+                            y: 260,
                             fieldLabel: 'Nombre de usuario',
                             id : 'nusuario'
                             
-                        },
-                        {
+                        }, {
                             xtype: 'textfield',
-                            x: 280,
-                            y: 170,
+                            x: 0,
+                            y: 300,
                             fieldLabel: 'Contraseña',
                             id : 'password'
                             
-                        },
-                        {
+                        },{
                             xtype: 'textfield',
-                            x: 280,
-                            y: 190,
+                            x: 0,
+                            y: 330,
                             fieldLabel: 'Repetir contraseña',
                             id : 'password2'
-                        },
-                        {
-                            xtype: 'button',
-                            x: 550,
-                            y: 160,
-                            text: 'Grabar'
-                            handler: function() {
- 
-            					Ext.Ajax.request({
-             					url: 'inicio/grabarcli',
+                        },{
+							xtype : 'button',
+							x : 440,
+							y : 300,
+							text : 'Registrar',
+							listeners: {
+								click:function(){
+								alert('Registro Existoso');
+								//Ext.Ajax.request({
+								//url: 'inicio/grabarcli',
              
             				  // ajax: 'true',
             					  // funcion: 'grabar',
-              					cedula: Ext.getCmp('cedula').getValue(), //obtiene el valor a traves del id del campo
-              					nombres: Ext.getCmp('nombres').getValue(),
-                				apellidos: Ext.getCmp('apellidos').getValue(),
-              					telefono: Ext.getCmp('telefono').getValue(),
-              					direccion: Ext.getCmp('direccion').getValue(),
-              					correo: Ext.getCmp('correo').getValue(),
-              					fecha_nacimiento: Ext.getCmp('fecha_nacimiento').getValue(),
-              					sexo: Ext.getComp('sexo').getValue(),
-              					nusuario: Ext.getComp('nusuario').getValue(),
-              					password: Ext.getComp('password').getValue(),
-             			},
-                        },
-                        {
-                            xtype: 'button',
-                            x: 550,
-                            y: 190,
-                            text: 'Cancelar'
-                        }
-                    ]
-                }
-            ]
-        });
+              					cedula: Ext.getCmp('cedula').getValue() //obtiene el valor a traves del id del campo
+              					nombres: Ext.getCmp('nombres').getValue()
+                				apellidos: Ext.getCmp('apellidos').getValue()
+              					telefono: Ext.getCmp('telefono').getValue()
+              					direccion: Ext.getCmp('direccion').getValue()
+              					correo: Ext.getCmp('correo').getValue()
+              					fecha_nacimiento: Ext.getCmp('fecha_nacimiento').getValue()
+              					sexo: Ext.getComp('sexo').getValue()
+              					nusuario: Ext.getComp('nusuario').getValue()
+              					password: Ext.getComp('password').getValue()
+             			
+								}
+			 			 	},
+			    			url:'cli_comprador'
+						},{
+							xtype : 'button',
+							x : 360,
+							y : 300,
+							text : 'Cancelar'
+							}
+		]
 
-        me.callParent(arguments);
-    }
-
-});
+	}); 
 	ventanaregistrarusuario= Ext.create('VentaRegistrarUsuario')
 		ventanaregistrarusuario.show();
 	
