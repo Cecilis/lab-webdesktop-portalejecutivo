2cfb63634c351551637bb8e94cbf5914b619f340
jmgv1989@gmail.com <jmgv1989@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-22 21:01:13 +0000

    clase usuario actualizada con registrar y una busqueda de id q aun no funciona bien

--- WebDesktopV1/app/models/usuario.rb
+++ WebDesktopV1/app/models/usuario.rb
@@ -1,15 +1,16 @@
 class Usuario < ActiveRecord::Base
-  def autenticarUsuario(nombre,password)
+    def autenticarUsuario(nombre,password)
      @usuario = Usuario.find(:first, :conditions => "nombre='#{nombre}' and password='#{password}'")
-    if @usuario!=nil
-      $tirajson = @usuario.to_json
-      valor = 1
-    else
-      $tirajson = '{ "success": "true", "exito": "false", "msg": " no existe!" }'
-      valor = 0
-    end 
+      if @usuario!=nil
+        $tirajson = @usuario.to_json
+        valor = 1
+      else
+        $tirajson = '{ "success": "true", "exito": "false", "msg": " no existe!" }'
+        valor = 0
+      end 
     return valor
   end
+  
   def buscarUsuarioLo(nombre)
      @usuario = Usuario.find(:first, :conditions => "nombre='#{nombre}'")
     if @usuario!=nil
@@ -21,5 +22,31 @@ class Usuario < ActiveRecord::Base
     end 
     return valor
   end
-
+  
+  def grabar_usuario(nombre,password)
+    @usuario= Usuario.new
+    @usuario.nombre=nombre
+    @usuario.password=password
+    @usuario.rol_id= 1
+    @usuario.estatus='a'
+    @usuario.save
+     valor=1
+    $tirajson = '{ "success": "true", "exito": "true", "message": "Datos guardados satisfactoriamente!" }'
+    puts' graba tira de js usuario------------------------------------'
+    return retornarid(nombre)
+  end
+  
+  def retornarid(nombre)
+     @usuario = Usuario.find(:first, :conditions => "nombre='#{nombre}'")
+     @identidad=''
+     puts' busca el usuairo por nombre --------------------------'+ nombre +'----------'
+      if @usuario!=nil
+        identidad = @usuario.id
+          
+        else  identidad=''
+      end
+      return identidad
+      puts' busca el ' + identidad+''
+  end
+  
 end
