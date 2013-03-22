c19d818487f79768e2e5372150856199c70142f0
jmgv1989@gmail.com <jmgv1989@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-22 21:10:45 +0000

    actualizado el modelo comprador

--- WebDesktopV1/app/models/comprador_vehiculo.rb
+++ WebDesktopV1/app/models/comprador_vehiculo.rb
@@ -11,22 +11,27 @@ class Comprador_Vehiculo < ActiveRecord::Base
     end 
     return valor
   end
-  def grabarComprador(cedula,nombres,apellidos,telefono,direccion,correo,fecha_nacimiento,sexo)
-    puts "pasehola"
-    @comprador =  Comprador_Vehiculo.find(:first, :conditions => "cedula='#{cedula}'")
-    if @comprador!=nil
-       @comprador.cedula=cedula
-       @comprador.nombres=nombres
-       @comprador.apellidos=apellidos
-       @comprador.telefono=telefono
-       @comprador.direccion=direccion
-       @comprador.correo=correo
-       @comprador.fecha_nacimiento=fecha_nacimiento
-       @comprador.sexo=sexo
-       @comprador.save
-    end
+
+  
+  #lo nuevo
+  def grabar_comprador(cedula,nombres,apellidos,telefono,direccion,correo,fecha_nacimiento,sexo,usuarios_id)
+     puts "**********************************************************************modelo"
+    @comprador_vehiculo=Comprador_Vehiculo.new
+    @comprador_vehiculo.cedula=cedula
+    @comprador_vehiculo.nombres=nombres
+    @comprador_vehiculo.apellidos=apellidos
+    @comprador_vehiculo.telefono=telefono
+    @comprador_vehiculo.direccion=direccion
+    @comprador_vehiculo.correo=correo
+    @comprador_vehiculo.fecha_nacimiento=fecha_nacimiento
+    @comprador_vehiculo.sexo=sexo
+    @comprador_vehiculo.usuarios_id=usuario_id
+    @comprador_vehiculo.save
     valor=1
     $tirajson = '{ "success": "true", "exito": "true", "message": "Datos guardados satisfactoriamente!" }'
 
   end
+
+ 
+
 end
