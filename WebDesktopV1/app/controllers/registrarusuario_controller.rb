530d6c08b6ada395b36efc44e984d938626b070c
jmgv1989@gmail.com <jmgv1989@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-22 21:03:09 +0000

    controlador de registrarusuario, ya guarda los datos de login pero aun no guarda los datos del compador de vehiculos

--- WebDesktopV1/app/controllers/registrarusuario_controller.rb
+++ WebDesktopV1/app/controllers/registrarusuario_controller.rb
@@ -2,5 +2,51 @@ class RegistrarusuarioController < ApplicationController
   def index
   end
   
+  #lo nuevo
+  
+  def grabar_comprador
+    @comprador_vehiculo=Comprador_Vehiculo.new
+    puts '+++++++++++++++++++++++++++++++++++++++++++++++++++++++controler'
+ 
+ 
+    @cedula=params[:cedula]
+    @nombres=params[:nombres]
+    @apellidos=params[:apellidos]
+    @telefono=params[:telefono]
+    @direccion=params[:direccion]
+    @correo=params[:correo]
+    @fecha_nacimiento=params[:fecha_nacimiento]
+    @sexo=params[:sexo]
+    #------
+    @nusuario=params[:nusuario]
+    @password=params[:password]
+    puts ''+ @nusuario +  ''
+    @id=grabar_usuario(@nusuario, @password);
+    
+    
+    puts '+++++++++++++++++++++++++++++++++++++++++++++++++++++++metio usuario'
+    
+    @usuarios_id=id
+    @comprador_vehiculo.grabar_comprador(@cedula,@nombres,@apellidos,@telefono,@direccion,@correo,@fecha_nacimiento,@sexo,@usuarios_id)
+    render :text => $tirajson
+  end
+  
+  #def buscar_comprador
+  #  @cv=Comprador_Vehiculo.new
+ #  nombres = params[:nombres]
+  # valor = @marca.busca_comprador_existe(nombre)
+   #render :text => $tirajson
+  #end
+  
+  #la parte del usuario
+  def grabar_usuario(nusuario, password)
+    @usuario=Usuario.new
+    @nombre=nusuario
+    @password=password
+    render :text => $tirajson
+     puts '+++++++++++++++++++++++++++++llama el guardar del modelo usuario'
+    return @usuario.grabar_usuario(@nombre,@password)
+  end
+  
   
 end
