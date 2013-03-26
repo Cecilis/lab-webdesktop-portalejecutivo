c50b440eeeff9edb05781316a73019b15d39bf58
MariaAPaezT@gmail.com <MariaAPaezT@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-26 23:15:44 +0000

    Agregados metodos para buscar los nombres de la ciudad y el estado.ya que se usa para la lista de concesionario

--- WebDesktopV1/app/models/estados.rb
+++ WebDesktopV1/app/models/estados.rb
@@ -1,3 +1,15 @@
 class Estados < ActiveRecord::Base
-
+  
+  def buscarnombreEstado(id_estado)
+    @estados =  Estados.find(:first, :conditions => "id='#{id_estado}'")
+    if @estados!=nil
+      $tirajson = @estados.to_json
+      valor = 1
+    else
+      $tirajson = '{ "success": "true", "exito": "false", "msg": " no existe!" }'
+      valor = 0
+    end 
+    puts $tirajson
+    return valor
+  end
 end
