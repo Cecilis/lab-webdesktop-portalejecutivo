204bdd098d4845d8e8fb8b59ccfcb0556ef3a666
jmgv1989@gmail.com <jmgv1989@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-26 14:32:51 +0000

    registrar usuario ya valida todos los campos y si ya se uso el usuario, lo unico es q cuando se quiere registrar con un usuario ya existente mandara un mensaje de registro exitoso aunue en realidad no guarda nada hasta q todos los campos sean validos

--- trunk/WebDesktopV1/public/javascripts/principal/registrarusuario.js
+++ trunk/WebDesktopV1/public/javascripts/principal/registrarusuario.js
@@ -1,32 +1,91 @@
-alert("se llamo");
+//alert("se llamo");
 
-Ext.require(['Ext.tree.*', 'Ext.data.*', 'Ext.tip.*', 'Ext.container.Viewport', 'Ext.container.ButtonGroup', 'Ext.panel.Panel']);
+Ext.require(['Ext.tree.*', 'Ext.data.*', 'Ext.tip.*', 'Ext.container.Viewport', 'Ext.container.ButtonGroup']);
 
+
+/*	
+var sexoo = new Ext.data.SimpleStore({
+			fields : ['id', 'sexoo'],
+			data : [['1', 'Masculino'], ['2', 'Femenino']]
+		});*/
+	var modelo = new Ext.data.SimpleStore({
+			fields : ['id', 'modelo'],
+			data : [['1', 'Femenino'], ['2', 'Masculino']]
+		});
+			
+		function verificaredad(){
+	
+		            var currentDate = new Date();
+		            // fn = new Date();
+		           var fn = Ext.getCmp('fecha_nacimiento').getValue();
+		            
+		           // alert("entre 1234" + fn + "xx");
+		            //alert("entre 1234" + currentDate + "xx");
+	                var day=currentDate.getDate();
+	                var fd=fn.getDate();
+				    // el mes es devuelto entre 0 y 11
+				    var month = currentDate.getMonth()+1;
+				    var fm = fn.getMonth()+1;
+				    
+				    var year= currentDate.getFullYear();
+				   	var fa= fn.getFullYear();
+				   	
+			        var pa = false;
+			        
+				    if((year-fa)<18 ){
+				    	//alert(" año menor" + year + "-"+ fa);
+				    	pa = false	
+				    }
+				    else{
+				    	if((year-fa)== 18)
+				    	{
+				    	//alert(" año igual");
+				    		if(fm > month) 
+				    		{//alert(" mes menor");
+				    			pa =  false}
+				    		else{
+				    			if(fm == month){
+				    				//alert(" mes igual");
+				    				if(fd <= day){
+				    					//alert(" dia mayor");
+				    					pa =  true
+				    				}else{
+				    					//alert(" dia menor");
+				    					pa =  false
+				    				}
+				    				
+				    			}
+				    			else{
+				    				//alert(" mes mayor");
+				    				pa =  true
+				    			}
+				    		}
+				        }
+				        else {//alert(" año mayor");
+				        	pa = true}
+				    }    
+				    
+				    return pa
+				   
+	}
+	
 Ext.onReady(function() {
-
-	alert("paso a crearlo");
-
-	Ext.QuickTips.init();
-	Ext.define('VentaRegistrarUsuario', {
-		extend : 'Ext.window.Window',
-
-		height : 549,
-		id : 'ventanacomprador',
-		width : 661,
-		x : 380,
-		y : 225,
-		layout : {
+		Ext.QuickTips.init();
+		Ext.define('VentaRegistrarUsuario', {
+			extend : 'Ext.window.Window',
+       		
+			height : 549,
+			id : 'ventanacomprador',
+			width : 661,
+			x : 380,
+			y : 225,
+			layout : {
 			type : 'absolute'
-		},
-		title : 'Registrar comprador',
-		//initComponent : function() {
-		//aqui con este se daña
-
-		//var me = this;
-
-		//Ext.applyIf(me, {
-
-		items : [{
+			},
+			title : 'Registrar comprador',
+		
+		items : [
+		{
 			xtype : 'form',
 
 			autoRender : true,
@@ -37,111 +96,197 @@ Ext.onReady(function() {
 			},
 			bodyPadding : 10,
 			title : '',
+			
 			items : [
-				{
-				xtype : 'textfield',
-				x : 0,
-				y : 70,
-				width : 200,
-				fieldLabel : 'Cedula',
-				id : 'cedula'
+				{		
+		            xtype: 'filefield',
+		            x : 0,
+					y : 270,
+		            id: 'imagen',
+		            emptyText: 'Imagen',
+		            fieldLabel: 'Imagen',
+		            name: 'photo-path',
+		            buttonText: 'Seleccionar Archivo',
+		            buttonConfig: {
+		                iconCls: 'upload-icon'
+			        }
+			      },
+		
+		    {
+					xtype : 'textfield',
+					x : 0,
+					y : 70,
+					width : 200,
+					fieldLabel : 'Cedula',
+					id : 'cedula',
+					minLength : 1,
+					 vtype: 'alphanum',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 110,
-				width : 290,
-				fieldLabel : 'Nombres',
-				id : 'nombres'
+					xtype : 'textfield',
+					x : 0,
+					y : 110,
+					width : 290,
+					fieldLabel : 'Nombres',
+					id : 'nombres',
+					minLength : 1,
+					vtype: 'alpha',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 150,
-				width : 290,
-				fieldLabel : 'Apellidos',
-				id : 'apellidos'
+					xtype : 'textfield',
+					x : 0,
+					y : 150,
+					width : 290,
+					fieldLabel : 'Apellidos',
+					id : 'apellidos',
+					minLength : 1,
+					vtype: 'alpha',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 190,
-				width : 250,
-				fieldLabel : 'Telefono',
-				id : 'telefono'
+					xtype : 'textfield',
+					x : 0,
+					y : 190,
+					width : 250,
+					fieldLabel : 'Telefono',
+					id : 'telefono',
+					minLength : 1,
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'textfield',
-				x : 260,
-				y : 150,
-				fieldLabel : 'Correo',
-				id : 'correo'
+					xtype : 'textfield',
+					x : 260,
+					y : 150,
+					fieldLabel : 'Correo',
+					id : 'correo',
+					minLength : 1,
+					vtype: 'email',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'textfield',
-				x : 260,
-				y : 190,
-				fieldLabel : 'Repetir Correo',
-				id : 'correo2'
+					xtype : 'textfield',
+					x : 260,
+					y : 190,
+					fieldLabel : 'Repetir Correo',
+					id : 'correo2',
+					minLength : 1,
+					vtype: 'email',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 230,
-				width : 430,
-				fieldLabel : 'Direccion',
-				id : 'direccion'
+					xtype : 'textfield',
+					x : 0,
+					y : 230,
+					width : 430,
+					fieldLabel : 'Direccion',
+					id : 'direccion',
+					minLength : 1,
+					vtype: 'alphanum',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'datefield',
-				x : 260,
-				y : 70,
-				fieldLabel : 'Fecha de nacimiento',
-				id : 'fecha_nacimiento'
+					xtype : 'datefield',
+					x : 260,
+					y : 70,
+					fieldLabel : 'Fecha de nacimiento',
+					id : 'fecha_nacimiento',
+					minLength : 1,
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'combobox',
-				x : 260,
-				y : 110,
-				width : 200,
+					store : modelo,
+					xtype : 'combobox',
+					x : 260,
+					y : 110,
+					width : 200,
+					fieldLabel : 'Sexo',
+					minLength : 1,
+					mode : 'local',
+				
+				displayField : 'modelo',
 				fieldLabel : 'Sexo',
-				id : 'sexo'
+					id : 'sexo'
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 260,
-				fieldLabel : 'Nombre de usuario',
-				id : 'nusuario'
+					xtype : 'textfield',
+					x : 0,
+					y : 300,
+					fieldLabel : 'Nombre de usuario',
+					id : 'nusuario',
+					minLength : 1,
+					vtype: 'alphanum',
+					emptyText: 'Campo obligatorio'
 
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 300,
-				fieldLabel : 'Contraseña',
-				id : 'password'
+					xtype : 'textfield',
+					x : 0,
+					y : 340,
+					fieldLabel : 'Contraseña',
+					id : 'password',
+					minLength : 1,
+					vtype: 'alphanum',
+					emptyText: 'Campo obligatorio',
+					allowBlank: false,
+                    emptyText: 'Campo obligatorio'
 
 				}, {
-				xtype : 'textfield',
-				x : 0,
-				y : 330,
-				fieldLabel : 'Repetir contraseña',
-				id : 'password2'
+					xtype : 'textfield',
+					x : 240,
+					y : 330,
+					fieldLabel : 'Repetir contraseña',
+					id : 'password2',
+					minLength : 1,
+					vtype: 'alphanum',
+					emptyText: 'Campo obligatorio'
 				}, {
-				xtype : 'button',
-				x : 440,
-				y : 300,
-				text : 'Registrar',
-				listeners : {
-					click : function() {
-						alert('pse por el boton');
-
-						grabar_comprador();
-					}
-				},
-				//url:'inicio'
+					xtype : 'button',
+					x : 440,
+					y : 400,
+					text : 'Registrar',
+					id: 'registrar',
+					listeners : {
+						click : function() {
+							//alert('pse por el boton')
+							if(verificarcampos()){
+							
+								if ( verificarpassword()) {
+							
+									if(verificarcorreo()){
+									
+				                       if(verificaredad()){
+								       		grabar_comprador() ;
+									   		url:'inicio'}
+									   else{
+									  		alert("El usuario es menor de edad, no se va a registrar");
+									   }
+									}
+							    	else{
+							    		alert("Los correos no son iguales");
+							    	}
+									
+								}
+								else{
+									alert("Las contraseñas no son iguales");
+								};
+							}
+							else{
+							alert("Debe llenar todos los campos");
+							}
+							//ventanaregistrarusuario.close();
+							url:'inicio'
+						}
+				}
+				//aqui va a otro lugar
 				}, {
-				xtype : 'button',
-				x : 360,
-				y : 300,
-				text : 'Cancelar',
-				listeners : {
-					click : function() {
+					xtype : 'button',
+					x : 360,
+					y : 400,
+					text : 'Cancelar',
+					listeners : {
+						click : function() {
+							
+						
 						Ext.getCmp('formulariocomprador').getForm().reset();
+						}
 					}
 				}
-			}]
+		
+		
+		
+			]
 		}]
 
 		//} ) aplyif me
@@ -151,8 +296,37 @@ Ext.onReady(function() {
 	ventanaregistrarusuario = Ext.create('VentaRegistrarUsuario')
 	ventanaregistrarusuario.show();
 	
+	function buscar_usuario() {
+			Ext.Ajax.request({
+				url : 'registrarusuario/buscar_usuario',
+				params : {
+					ajax : 'true',
+					funcion : 'buscar_usuario',
+					nombre : Ext.getCmp('nusuario').getValue(),
+				},
+				
+				//Retorno exitoso de la pagina servidora a traves del formato JSON
+				success : function(exito, request) {
+					datos = Ext.JSON.decode(exito.responseText);
+					if (datos.exito == 'true') {
+						
+						Ext.Msg.alert( "El nombre de usuario ya existe.");
+						
+					} else {
+						
+						grabar_comprador();
+					}
+				},
+				//No hay retorno de la pagina servidora
+				failure : function() {
+				
+					Ext.Msg.alert("Error", "Servidor no conectado");
+
+				}
+			});
+	}
 	function grabar_comprador() {
-		alert('Registro Existoso');
+		alert('pase interno');
 			Ext.Ajax.request({
 				url : 'registrarusuario/grabar_comprador',
 				params : {
@@ -168,23 +342,55 @@ Ext.onReady(function() {
   					fecha_nacimiento: Ext.getCmp('fecha_nacimiento').getValue(),
   					sexo: Ext.getCmp('sexo').getValue(),
   					nusuario: Ext.getCmp('nusuario').getValue(),
-  					password: Ext.getCmp('password').getValue()
+  					password: Ext.getCmp('password').getValue(),
+  					imagen : Ext.getCmp('imagen').getValue(),
   					 
 				},
 				success : function(exito, request) {
 					datos = Ext.JSON.decode(exito, reponseText);
-					datos = Ext.JSON.decode(exito, reponseText);
-					 Ext.Msg.alert("Exito", "Se registro el usuario!!");
-					//Ext.getCmp('formulariomarca').getForm().reset();
+				
+					Ext.Msg.alert("Exito", "Se registro el usuario!!");
+					Ext.getCmp('formulariocomprador').getForm().reset();
 					//Ext.getCmp('btnregistrar').disable(false);
 					
 				},
 				failure : function() {
-					Ext.Msg.alert("Error", "Servidor NO Conectado!!");
+					//Ext.Msg.alert("Error", "Servidor NO Conectado!!");
 				}
 			//aqui estaban antes
-			
-		
-         });
+			//del header 
+			})
+         
+         alert('Registro Existoso');
+         Ext.getCmp('formulariocomprador').getForm().reset();
+         
        }
+       
+       function verificarpassword(){
+       	
+       		if(Ext.getCmp('password').getValue()== Ext.getCmp('password2').getValue())
+       			{ return true}
+       			else {return false}
+       }
+       
+        function verificarcorreo(){
+       		if(Ext.getCmp('correo').getValue()== Ext.getCmp('correo2').getValue())
+       			{return true}
+       			else {return false}
+       }
+       
+        function verificarcampos(){
+      	 	if( Ext.getCmp ('cedula').getValue()=="" || Ext.getCmp('nombres').getValue()=="" ||
+    			Ext.getCmp('apellidos').getValue()=="" || Ext.getCmp('telefono').getValue()==""||
+  				Ext.getCmp('direccion').getValue()==""|| Ext.getCmp('fecha_nacimiento').getValue()== null ||
+       			Ext.getCmp('sexo').getValue()== null || Ext.getCmp('nusuario').getValue()=="" ||
+       			Ext.getCmp('password').getValue()=="" || Ext.getCmp('password2').getValue()=="" ||
+           		Ext.getCmp('correo').getValue()== "" || Ext.getCmp('correo2').getValue()=="" ||
+           		Ext.getCmp('imagen').getValue()== ""
+            	)
+       		{return false}
+       		else {return true}
+       }
+       
+       
 })
