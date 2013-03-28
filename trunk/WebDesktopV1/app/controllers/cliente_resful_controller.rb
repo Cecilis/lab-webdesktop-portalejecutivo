024bc084a62285a92ab64b0f536e722d5cb3a846
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-28 05:54:32 +0000

    conexion con los servicios funciona, solo que hay que cambiarle la ip

---
+++ trunk/WebDesktopV1/app/controllers/cliente_resful_controller.rb
@@ -0,0 +1,14 @@
+class ClienteResfulController < ApplicationController
+  require'nestful'
+  def index
+
+  end
+  
+  def call_service
+    #ojo la ip hay que cambiarsela por la ip donde estan los servicios para que pueda funcionar
+      $tirajson= Nestful.get 'http://192.168.1.113:81/eai/servidor-restful/Despachador.php?servicio=5&rif=j-17946731', :format => :json #=> {:json_hash => 1}
+      render :text => $tirajson.to_json 
+      puts $tirajson
+      return $tirajson   
+  end
+end
