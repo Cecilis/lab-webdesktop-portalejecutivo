07b571cb8794937d2cc71311982a66fae89fde63
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-28 14:00:20 +0000

    busca todos los bancos en los servicos de EAI

--- trunk/WebDesktopV1/app/controllers/cliente_resful_controller.rb
+++ trunk/WebDesktopV1/app/controllers/cliente_resful_controller.rb
@@ -6,7 +6,7 @@ class ClienteResfulController < ApplicationController
   
   def call_service
     #ojo la ip hay que cambiarsela por la ip donde estan los servicios para que pueda funcionar
-      $tirajson= Nestful.get 'http://192.168.1.113:81/eai/servidor-restful/Despachador.php?servicio=5&rif=j-17946731', :format => :json #=> {:json_hash => 1}
+      $tirajson= Nestful.get 'http://192.168.1.113:81/eai/servidor-restful/Despachador.php?servicio=4',:format => :json #=> "body" #:format => :json #=> {:json_hash => 1}
       render :text => $tirajson.to_json 
       puts $tirajson
       return $tirajson   
