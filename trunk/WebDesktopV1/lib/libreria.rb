1ac8a5eb7418280bca007f55ec4a58aa8193dda5
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-28 22:36:28 +0000

    librerias para poder imprimir la proforma

---
+++ trunk/WebDesktopV1/lib/libreria.rb
@@ -0,0 +1,21 @@
+# /****************************************************************************/
+# /*                   Validar Sesion                                         */
+# /****************************************************************************/
+# /*
+#  Version    : 1.0 en ruby 1.8.x
+#  Fecha     : 28 de Marzo 2008
+#  Autor     : Edgar Gonzalez
+# */
+
+# def validarSesion
+   # user = session[:usuario]
+   # clave = session[:clave]
+   # @usuario = Usuarios.new
+   # @valor=@usuario.validarusuario(user,clave)
+   # if @valor==0
+    # session[:mensajeError] = "No tiene privilegios de acceder esta pagina"
+    # session[:vinculo] = $host+"inicio"
+    # redirect_to $host+'informacion'
+   # end
+   # return
+# end
