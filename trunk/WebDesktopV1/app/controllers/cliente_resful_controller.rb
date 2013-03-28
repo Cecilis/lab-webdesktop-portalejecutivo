6441ed93b392e35aabef016fa4c23edd13b4e374
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-28 22:29:36 +0000

    se le agrego localhost a la ip

--- trunk/WebDesktopV1/app/controllers/cliente_resful_controller.rb
+++ trunk/WebDesktopV1/app/controllers/cliente_resful_controller.rb
@@ -3,10 +3,9 @@ class ClienteResfulController < ApplicationController
   def index
 
   end
-  
   def call_service
     #ojo la ip hay que cambiarsela por la ip donde estan los servicios para que pueda funcionar
-      $tirajson= Nestful.get 'http://192.168.1.113:81/eai/servidor-restful/Despachador.php?servicio=4',:format => :json #=> "body" #:format => :json #=> {:json_hash => 1}
+      $tirajson= Nestful.get 'http://localhost/eai/servidor-restful/Despachador.php?servicio=4',:format => :json #=> "body" #:format => :json #=> {:json_hash => 1}
       render :text => $tirajson.to_json 
       puts $tirajson
       return $tirajson   
