56b9ae8f00fe746835c54cb1df102ae484172642
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-30 17:23:03 +0000

    busca los precios en indpabis

--- WebDesktopV1/app/controllers/cliente_resful_controller.rb
+++ WebDesktopV1/app/controllers/cliente_resful_controller.rb
@@ -10,4 +10,14 @@ class ClienteResfulController < ApplicationController
       puts $tirajson
       return $tirajson   
   end
+  def call_service2_precio_vehiculo
+    #este servicio se va a traer el precio de los vehiculos desde indepabis que estan regulados
+    #ojo la ip hay que cambiarsela por la ip donde estan los servicios para que pueda funcionar
+     @id_modelo=params[:id_modelo]
+      $tirajson= Nestful.get 'http://localhost/eai/servidor-restful/Despachador.php?servicio=21&id_modelo='+@id_modelo,:format => :json #=> "body" #:format => :json #=> {:json_hash => 1}
+      render :text => $tirajson.to_json
+      puts $tirajson
+      puts'******************* despues de la tira Json'
+      return $tirajson   
+  end
 end
