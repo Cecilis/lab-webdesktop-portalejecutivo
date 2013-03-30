7a51e913728c7f5dbd76a57cfea8f23672b338f8
MariaAPaezT@gmail.com <MariaAPaezT@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-30 08:24:03 +0000

    Modificado lo indicadores que vengan de la base de datos,y se filtren por rol, falta programa el boton de detalle para que muestre los detalles del indicador

--- WebDesktopV1/public/javascripts/menu_ensambladora/menu_ensambladora.js
+++ WebDesktopV1/public/javascripts/menu_ensambladora/menu_ensambladora.js
@@ -1,26 +1,23 @@
-  //Calcular las posiciones x,y segun la resolucion del monitor del cliente para el tabpanel por ser un layout absolute
-   var posx = 0;
-   var posy = 0;
-   var factorw = 0.001;
-   var factorh = 0.10;
-   //1024x768
-   if (screen.width==1024 && screen.height==768)
-   {
-    factorw = 0.025;
-    factorh = 0.10;
-   }
-   //1280x800
-   if (screen.width==1280 && screen.height==800)
-   {
-    factorw = 0.080;
-    factorh = 0.10;
-   }
-   //1280x1024
-   if (screen.width==1280 && screen.height==1024)
-   {
-    factorw = 0.020;
-    factorh = 0.005;
-   }
+//Calcular las posiciones x,y segun la resolucion del monitor del cliente para el tabpanel por ser un layout absolute
+var posx = 0;
+var posy = 0;
+var factorw = 0.001;
+var factorh = 0.10;
+//1024x768
+if (screen.width == 1024 && screen.height == 768) {
+	factorw = 0.025;
+	factorh = 0.10;
+}
+//1280x800
+if (screen.width == 1280 && screen.height == 800) {
+	factorw = 0.080;
+	factorh = 0.10;
+}
+//1280x1024
+if (screen.width == 1280 && screen.height == 1024) {
+	factorw = 0.020;
+	factorh = 0.005;
+}
 posx = parseInt(screen.width * factorw);
 posy = parseInt(screen.height * factorh);
 
@@ -28,32 +25,14 @@ Ext.require(['Ext.tree.*', 'Ext.data.*', 'Ext.tip.*', 'Ext.container.Viewport', 
 Ext.require('Ext.chart.*');
 Ext.require(['Ext.Window', 'Ext.layout.container.Fit', 'Ext.fx.target.Sprite', 'Ext.window.MessageBox']);
 
-
 Ext.onReady(function() {
 	Ext.QuickTips.init();
-	
-	 // var bd = Ext.create('Ext.data.TreeStore', {
-		 // proxy : {
-			 // type : 'ajax',
-			 // url : 'menu_ensambladora/generar_menu?tipo=3'
-		 // },
-		 // root : {
-			 // text : 'Portal Ejecutivo',
-			 // id : 'root_node',
-			 // expanded : true,
-		 // },
-		 // folderSort : false,
-		 // sorters : [{
-			 // property : 'id',
-			 // direction : 'ASC'
-		 // }]
-	 // });
 
 	var store = Ext.create('Ext.data.TreeStore', {
 		proxy : {
 			type : 'ajax',
 			url : 'menu_ensambladora/generar_menu?tipo=1'
-			},
+		},
 		root : {
 			text : 'Ensambladora',
 			id : 'root_node',
@@ -65,21 +44,23 @@ Ext.onReady(function() {
 			direction : 'ASC'
 		}]
 	});
-	
+
 	ventana = Ext.create('miVentanalista');
-                  ventana.show();
+	ventana.show();
 
 	ventanaindicadores = Ext.create('indicadoreseje');
-                  ventanaindicadores.show();
+	buscar_usuarioLo();
+	buscar_indicador();
+	ventanaindicadores.show();
 
 	// ventana = Ext.create('catalogo');
-                  // ventana.show();
+	// ventana.show();
 
 	var tree = Ext.create('Ext.tree.Panel', {
 		store : store,
 		// store : bd,
 		title : 'Opciones',
-	    //layout: 'accordion',
+		//layout: 'accordion',
 		renderTo : 'tree_el',
 		height : 350,
 		width : 320,
@@ -98,32 +79,32 @@ Ext.onReady(function() {
 		useArrows : true,
 		listeners : {
 			itemclick : function(view, node) {
-				if(node.get('text') == "Registrar o Modificar Nuevos Vehiculos"){
+				if (node.get('text') == "Registrar o Modificar Nuevos Vehiculos") {
 					ventana.close();
 					ventana = Ext.create('miVetanaRegistrar');
-                  	ventana.show();
+					ventana.show();
 				}
-				if(node.get('text') == "Listado de Pedidos por Concesionario"){
+				if (node.get('text') == "Listado de Pedidos por Concesionario") {
 					ventana.close();
 					ventana = Ext.create('miVentanalista');
-                  	ventana.show();
+					ventana.show();
 				}
-				if(node.get('text')=='Configurar Nuevo Indicador'){
+				if (node.get('text') == 'Configurar Nuevo Indicador') {
 					ventana.close();
 					ventana = Ext.create('mipanelejecutivo', {
 						renderTo : 'tree_el',
 						title : node.get('text'),
-					}); 
+					});
 				}
 				if (node.get('text') == "Registrar Modelo de Vehiculo") {
 					ventana.close();
 					ventana = Ext.create('miVentanaModelo');
-                  	ventana.show();
+					ventana.show();
 				}
 				if (node.get('text') == "Registrar Caracteristica de un Modelo") {
 					ventana.close();
 					ventana = Ext.create('miVentanaCaracteristica');
-                  	ventana.show();
+					ventana.show();
 				};
 			}
 		}
