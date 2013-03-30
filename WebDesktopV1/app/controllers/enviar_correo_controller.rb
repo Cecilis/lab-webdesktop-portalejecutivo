f8bc0e8dd50346e8b08134b0a282002564b87e6d
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-30 20:08:13 +0000

    envia un correo a la persona que realizo la proforma y le envio  todos los datos de la operacion

--- WebDesktopV1/app/controllers/enviar_correo_controller.rb
+++ WebDesktopV1/app/controllers/enviar_correo_controller.rb
@@ -4,9 +4,19 @@ class EnviarCorreoController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:ipn_notification]
   def index
   end
-  def enviarcorreo
+  def enviarcorreo  #enviar correo con todos los datos de la persona que genera la proforma
+    @nombres=params[:nombre]
+    @apellido=params[:apellido]
+    @telefono=params[:telefono]
+    @fecha_generada=params[:fecha_generada]
+    @marca=params[:marca]
+    @modelo=params[:modelo]
+    @color=params[:color]
+    @costo_vehiculo=params[:costo_vehiculo]
+    @transmision=params[:transmision]
+    @tapiceria=params[:tapiceria]
+    @banco=params[:banco]
     @email=params[:correo]
-    puts @email
     Pony.mail(:to => @email, :via => :smtp, :via_options => {
     :address => 'smtp.gmail.com',
     :port => '587',
@@ -16,8 +26,29 @@ class EnviarCorreoController < ApplicationController
     :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain => "pop.gmail.com", # don't know exactly what should be here
     },
-    :subject => '****SIAEVEV****Se ha Generado la Proforma', :body => 'SIAVEV ----> Su Proforma ha sido Generada Satisfactoriamente!!
-    Su tiempo de espera son alrededor de 30 dias,para la aprobacion de su Credito')
+    :subject => '****SIAEVEV****Se ha Generado la Proforma', :body => '
+    SIAVEV ----> Su Proforma ha sido Generada Satisfactoriamente!!
+    Su tiempo de espera son alrededor de 30 dias,para la aprobacion de su Credito
+    -----------------------------------------------------------------------------
+     '+@nombres+' '+@apellido+' '+' tu Proforma se ha enviadoa a ****SIAEVEV****
+    -----------------------------------------------------------------------------
+    Nombre(S) :'+@nombres+'
+    Apellido(S) :'+@apellido+' 
+    fecha Solicitud Proforma:'+@fecha_generada+' 
+    Email:'+@email+'
+    ****************DATOS DEL VEHICULO*******************************************
+    Marca del Vehiculo:'+@marca+' 
+    Modelo Vehiculo:'+@modelo+'
+    Color Vehiculo:'+@color+'
+    Costo Vehiculo:'+@costo_vehiculo+' 
+    Transmision:'+@transmision+' 
+    Tapiceria :'+@color+'
+    ***************BANCO A SOLICITAR CREDITO*************************************
+    Banco :'+@banco+'
+    -----------------------------------------------------------------------------
+    Para logearte en tu cuenta con el siguiente link
+    http://localhost:3000/inicio
+    Para mas informacion Llamar al 0500-SIAEVEV')
   end 
   def enviarcorreo_nuevousuario
     @nombres=params[:nombres]
@@ -59,7 +90,23 @@ class EnviarCorreoController < ApplicationController
     :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain => "pop.gmail.com", # pop.gmail.com
     },
-    :subject => '****SIAEVEV****Se ha Creado un Nuevo Usuario', :body => 'SIAVEV ----> Su Cuenta de Usuario ha sido Generada Satisfactoriamente!!
+    :subject => '****SIAEVEV****Se ha Creado un Nuevo Indicador', :body => 'SIAVEV ----> Su Cuenta de Usuario ha sido Generada Satisfactoriamente!!
+    Bienvenido a ****SIAEVEV****')
+  end 
+  def enviarcorreo_nuevoindicador
+    @nombres=params[:nombres]
+    @apellidos=params[:apellidos]
+    @email=params[:correo] #correo del destinatario que llega por parametros ajax
+    Pony.mail(:to => @email, :via => :smtp, :via_options => {
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :enable_starttls_auto => true,
+    :user_name => 'laboratoriotresdidactico', #usuario gmail
+    :password => 'laboratorio123',            #password gmail
+    :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain => "pop.gmail.com", # pop.gmail.com
+    },
+    :subject => '****SIAEVEV****Se ha Creado un Nuevo Indicador', :body => 'SIAVEV ----> Su Cuenta de Usuario ha sido Generada Satisfactoriamente!!
     Bienvenido a ****SIAEVEV****')
   end 
 end
