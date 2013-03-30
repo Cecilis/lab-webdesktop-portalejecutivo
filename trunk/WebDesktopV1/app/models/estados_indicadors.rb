ddbf0eb436491f17f17fe5bac606961cd8fbbec2
MariaAPaezT@gmail.com <MariaAPaezT@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-30 18:57:26 +0000

    Modificado del porta ejecutivo los indicadores aun no funciona completamente

--- trunk/WebDesktopV1/app/models/estados_indicadors.rb
+++ trunk/WebDesktopV1/app/models/estados_indicadors.rb
@@ -1,3 +1,15 @@
 class Estados_Indicadors < ActiveRecord::Base
-  
+  #Ma.Ale
+  def buscarEstadoIndi(usuario_indica_id)
+    @estados_Indicadors =  Estados_Indicadors.find(:first, :conditions => "id_estado='#{1}'")
+    if @estados_Indicadors!=nil
+      $tirajson = @usuarios.to_json
+      valor = 1
+    else
+      $tirajson = '{ "success": "true", "exito": "false", "msg": " no existe!" }'
+      valor = 0
+    end 
+    puts 'tira estado '+$tirajson
+    return @estados_Indicadors
+  end
 end
