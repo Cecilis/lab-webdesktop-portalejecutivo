a09b17fc2a378db9ad4434c6fdce35891bfdaab1
mariaapaezt@gmail.com <mariaapaezt@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-31 10:10:03 +0000

    genera la lista de ensambladoras

--- WebDesktopV1/public/javascripts/menu_admin/lista_emsambladora.js
+++ WebDesktopV1/public/javascripts/menu_admin/lista_emsambladora.js
@@ -27,6 +27,7 @@ posy = parseInt(screen.height * factorh);
 var data = null;
 var ventana = null;
 var boton = null;
+var idestado_ensa= null;
 
 Ext.require([
 	         	'Ext.tree.*',
@@ -37,23 +38,36 @@ Ext.require([
     			'Ext.panel.Panel'
 	         ]);
 
-//Definicion del Modelo
- Ext.define('Ensambladora', {
-    extend: 'Ext.data.Model',
-    fields: [ 'marca', 'ensambladora']
+//Definicion del Data model Ensambladora
+Ext.define('Ensambladora', {
+ extend: 'Ext.data.Model',
+           fields: [
+            {name: 'id_ensam', type: 'int'},
+            {name: 'rif_ensam', type: 'varchar'},
+            {name: 'nombre_ensam', type: 'varchar'},
+            {name: 'direccion_ensam', type: 'varchar'},
+            {name: 'telefono_ensam', type: 'varchar'},
+            {name: 'correo_ensam', type: 'varchar'},
+            {name: 'ciudads_id_ensam', type: 'varchar'},
+            {name: 'usuarios_id_ensam', type: 'int'},
+            {name: 'marcas_id_ensam', type: 'int'},
+           ],
 });
 
-//Definicion del Data Store
-var ensambladoraStore = Ext.create('Ext.data.Store', {
+//Definicion del Data Store Ensambladora
+ ensambladoraStore = Ext.create('Ext.data.Store', {
     model: 'Ensambladora',
-    data: [
-        { marca: 'Ford', ensambladora : 'Ford Ensambladora'},
-        { marca: 'Fiat', ensambladora : 'Fiat Ensambladora'},
-        { marca: 'Huyndai', ensambladora : 'Huyndai Ensambladora'},
-        { marca: 'Daewood', ensambladora : 'Daewood Ensambladora'},
-        { marca: 'Chevrolet', ensambladora : 'Chevrolet Ensambladora'},
-    ]
+    autoLoad: true,
+    proxy: {
+         type: 'ajax',
+         url : 'menu_admin/generardatalistaEnsambladora',
+         reader: {
+                  type: 'json',
+                  root: 'datos'
+              }
+           }
 });
+
 
 //Definicion de la clase UsuariosGrid
 Ext.define('App.EnsambladoraGrid', {
@@ -66,8 +80,11 @@ Ext.define('App.EnsambladoraGrid', {
         //Definicion de las columnas del grid
         this.columns = [
             {xtype: 'rownumberer', width: 20, sortable: true},
-            {text: "Marca", width: 60, dataIndex: 'marca', sortable: true},
-            {text: "Ensambladora", width: 100, dataIndex: 'ensambladora', sortable: true},
+            {text: "Rif", width: 60, dataIndex: 'rif_ensam', sortable: true},
+            {text: "Nombre", width: 100, dataIndex: 'nombre_ensam', sortable: true},
+            {text: "Dirección", width: 100, dataIndex: 'direccion_ensam', sortable: true},
+            {text: "Telefono", width: 100, dataIndex: 'telefono_ensam', sortable: true},
+            {text: "Correo", width: 100, dataIndex: 'correo_ensam', sortable: true},
         ];
         // this.dockedItems = [ {
 	  		// xtype: 'pagingtoolbar',
@@ -85,12 +102,101 @@ Ext.define('App.EnsambladoraGrid', {
         this.listeners = {
                           itemclick : function(view) {
                            data = this.getSelectionModel().selected.items[0].data;
-                  			 ventanalista.close();
+                  			 asignarDatosEnsambladora();
+                           	 buscar_nombreCiudadEnsa();
                           }
                          };
         //Llamamos a la super clase a iniciacion del componente
         App.EnsambladoraGrid.superclass.initComponent.call(this);
     }
+});
+
+Ext.define('ventanatabensambladoragrid', {
+    extend: 'Ext.tab.Panel',
+	alias: 'widget.tabinformensambladoragrid',
+    x: 0,
+    y: 175,
+    height: 500,
+    width: 650,
+    activeTab: 0,
+
+    initComponent: function() {
+        var me = this;
+
+        Ext.applyIf(me, {
+            items: [
+                {
+                    xtype: 'panel',
+                    layout: {
+                        type: 'absolute'
+                    },
+                    title: 'Ensambladora',
+					items: [
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 20,
+                                    id: 'rif_ensa',
+                                    disabled: true,
+                                    fieldLabel: 'Rif:'
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 60,
+                                    id: 'nombre_ensa',
+                                    disabled: true,
+                                    fieldLabel: 'Nombre'
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 100,
+                                    id: 'direccion_ensa',
+                                    disabled: true,
+                                    fieldLabel: 'Dirección'
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 140,
+                                    id: 'telefono_ensa',
+                                    disabled: true,
+                                    fieldLabel: 'Telefono'
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 180,
+                                    id: 'correo_ensa',
+                                    width: 310,
+                                    disabled: true,
+                                    fieldLabel: 'Correo'
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 220,
+                                    id: 'ciudad_ensa',
+                                    disabled: true,
+                                    fieldLabel: 'Ciudad'
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    x: 100,
+                                    y: 260,
+                                    id: 'estado_ensa',
+                                    disabled: true,
+                                    fieldLabel: 'Estado'
+                                }
+                            ]
+                }
+            ]
+        });
+
+         me.callParent(arguments);
+    }
+
 });
 
 //Definicion de la ventana contendora del grid
@@ -101,7 +207,7 @@ Ext.define('miVentanalistaEnsambladora', {
                 x: 350,
                 y: 30,
                 width       : 650,
-                height      : 575,
+                height      : 600,
                 closeAction :'hide',
                 plain       : true,
                 closable    : true,
@@ -110,7 +216,7 @@ Ext.define('miVentanalistaEnsambladora', {
                 maximizable : false,
                 minimizable : false,
                 modal       : false,
-                title       : 'Listados de Ensambladoras',
+                title       : 'Listados de Ensambladora',
                 buttonAlign : 'center',
                 constrain   : true,
         		collapsible: true,
@@ -129,7 +235,7 @@ Ext.define('miVentanalistaEnsambladora', {
                                 type: 'absolute'
                             },
                     		header: true,
-                    		title: 'Lista de Ensambladoras',
+                    		title: 'Lista de Ensambladora',
                     		items: [
 	                    		{ 
 	                 			xtype:'ensambladoragrid',
@@ -137,11 +243,76 @@ Ext.define('miVentanalistaEnsambladora', {
 	                 			viewConfig: {
 	                    		   }
 	                 		    },
+	                 		    {
+				                  xtype:'tabinformensambladoragrid',
+				                 	viewConfig: {
+			                    		}
+				                },
                     		]
 		                 },    		          		    
                 ],
-                   });
+                   });           
 
         me.callParent(arguments);
     }
-   });
+});
+   //Metodo para buscar y mostrar el nombre de la ciudad
+   function buscar_nombreCiudadEnsa() {
+			Ext.Ajax.request({
+				url : 'menu_admin/buscar_nombreCiudad',
+				params : {
+					ajax : 'true',
+					funcion : 'buscar_nombreCiudad',
+					idciudad : data.ciudads_id_ensam,
+				},
+				//Retorno exitoso de la pagina servidora a traves del formato JSON
+				success : function(exito, request) {
+					datos = Ext.JSON.decode(exito.responseText);
+					if (datos.exito == 'false') {
+						Ext.Msg.alert("Error", datos.msg);
+					} else {
+						Ext.getCmp('ciudad_ensa').setValue(datos.nombre);
+						idestado_ensa = datos.estados_id;
+						buscar_nombreEstadoEnsa();
+					}
+				},
+				//No hay retorno de la pagina servidora
+				failure : function() {
+					Ext.Msg.alert("Error", "Servidor no conectado");
+
+				}
+			});
+};  
+
+//Metodo para buscar y mostrar el nombre del estado
+   function buscar_nombreEstadoEnsa() {
+			Ext.Ajax.request({
+				url : 'menu_admin/buscar_nombreEstado',
+				params : {
+					ajax : 'true',
+					funcion : 'buscar_nombreEstado',
+					id_estado : idestado_ensa,
+				},
+				//Retorno exitoso de la pagina servidora a traves del formato JSON
+				success : function(exito, request) {
+					datos = Ext.JSON.decode(exito.responseText);
+					if (datos.exito == 'false') {
+						Ext.Msg.alert("Error", datos.msg);
+					} else {
+						Ext.getCmp('estado_ensa').setValue(datos.nombre);
+					}
+				},
+				//No hay retorno de la pagina servidora
+				failure : function() {
+					Ext.Msg.alert("Error", "Servidor no conectado");
+				}
+			});
+}; 
+
+function asignarDatosEnsambladora () {
+  Ext.getCmp('rif_ensa').setValue(data.rif_ensam);
+  Ext.getCmp('nombre_ensa').setValue(data.nombre_ensam);
+  Ext.getCmp('direccion_ensa').setValue(data.direccion_ensam);
+  Ext.getCmp('telefono_ensa').setValue(data.telefono_ensam);
+  Ext.getCmp('correo_ensa').setValue(data.correo_ensam);
+};
