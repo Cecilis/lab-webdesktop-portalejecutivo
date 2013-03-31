6476ce091e58adbec453d4d29b88d244d379a2ba
MariaAPaezT@gmail.com <MariaAPaezT@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-31 02:28:07 +0000

    Agregado del portal ejecutivo los indicadores. hay problema con el actor concesionario porque no se trae sus indicadores

--- trunk/WebDesktopV1/app/models/indicadors.rb
+++ trunk/WebDesktopV1/app/models/indicadors.rb
@@ -1,3 +1,16 @@
 class Indicadors < ActiveRecord::Base
-  
+  #Ma.Ale
+  def buscarIndicadors(usuario_indica_id)
+    @indi =  Indicadors.find(:first, :conditions => "id='#{usuario_indica_id}'")
+    if @indi!=nil
+      $tira_indjson = @indi.to_json
+      valor = 1
+    else
+      $tira_indjson = '{ "success": "true", "exito": "false", "msg": " no existe!" }'
+      valor = 0
+    end 
+    puts 'tira rol '+$tira_indjson
+    # return @rols
+    return valor
+  end
 end
