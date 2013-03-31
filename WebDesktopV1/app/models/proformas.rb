c5bb358eae3e53fe81dd4f35b7ae6e32c8a6929b
BLUMXP@gmail.com <BLUMXP@gmail.com@af4e4472-0eb5-a565-1df6-3e8cb8583262>
2013-03-31 12:37:38 +0000

    actualizada

--- WebDesktopV1/app/models/proformas.rb
+++ WebDesktopV1/app/models/proformas.rb
@@ -11,5 +11,15 @@ class Proformas < ActiveRecord::Base
     end 
     return @proformas
   end
-  
+  def graba_proforma(fecha,validez,estatus,banco,comprador_vehiculo_id,concesionario,detalle_vehiculo_id)
+    @proforma=Proformas.new
+    @proforma.fecha=fecha
+    @proforma.validez=validez
+    @proforma.estatus=estatus
+    @proforma.bancos_id=banco
+    @proforma.comprador_vehiculos_id=comprador_vehiculo_id
+    @proforma.concesionario_vehiculos_id=concesionario
+    @proforma.detalle_vehiculos_id=detalle_vehiculo_id
+    @proforma.save  
+  end
 end
